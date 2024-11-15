3160754ee4703ecd412497ea46b2cf4f302906d9
Mạnh Vũ <vubamanh05@gmail.com>
2024-11-15 18:26:57 +0700

    Add Unit tests to WishlistService

---
+++ backend/services/WishlistService/WishlistService.API/Infrastructure/Repositories/CourseRepository.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Driver;
+using WishlistService.API.Models;
+
+namespace WishlistService.API.Infrastructure.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly IMongoCollection<Course> _courseCollection;
+
+        public CourseRepository(IMongoCollection<Course> courseCollection)
+        {
+            _courseCollection = courseCollection;
+        }
+
+        public async Task<Course?> FindByIdAsync(string id, CancellationToken cancellationToken = default)
+        {
+            var filter = Builders<Course>.Filter.Eq(c => c.Id, id);
+
+            return await _courseCollection.Find(filter).FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public async Task InsertCourseAsync(Course course)
+        {
+            await _courseCollection.InsertOneAsync(course);
+        }
+
+        public async Task UpdateCourseAsync(Course course)
+        {
+            var filter = Builders<Course>.Filter.Eq(c => c.Id, course.Id);
+
+            await _courseCollection.ReplaceOneAsync(filter, course);
+        }
+
+        public async Task UpdateStatusAsync(string courseId, CourseStatus status)
+        {
+            await _courseCollection.UpdateOneAsync(
+                Builders<Course>.Filter.Eq(c => c.Id, courseId),
+                Builders<Course>.Update.Set(c => c.Status, status)
+            );
+        }
+    }
+}
---
+++ backend/services/WishlistService/WishlistService.API/Infrastructure/Repositories/WishlistRepository.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Driver;
+using WishlistService.API.Models;
+
+namespace WishlistService.API.Infrastructure.Repositories
+{
+    public class WishlistRepository : IWishlistRepository
+    {
+        private readonly IMongoCollection<Wishlist> _wishlistCollection;
+
+        public WishlistRepository(IMongoCollection<Wishlist> wishlistCollection)
+        {
+            _wishlistCollection = wishlistCollection;
+        }
+
+        public async Task<Wishlist?> FindByUserIdAsync(string userId, CancellationToken cancellationToken = default)
+        {
+            var filter = Builders<Wishlist>.Filter.Eq(x => x.UserId, userId);
+
+            return await _wishlistCollection.Find(filter).FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public async Task InsertWishlistAsync(Wishlist wishlist)
+        {
+            await _wishlistCollection.InsertOneAsync(wishlist);
+        }
+
+        public async Task UpdateWishlistAsync(Wishlist wishlist)
+        {
+            var filter = Builders<Wishlist>.Filter.Eq(x => x.Id, wishlist.Id);
+
+            await _wishlistCollection.ReplaceOneAsync(filter, wishlist);
+        }
+
+        public async Task UpdateCourseInWishlistsAsync(Course course)
+        {
+            var filter = Builders<Wishlist>.Filter.Eq("Courses._id", course.Id);
+            var update = Builders<Wishlist>.Update.Set("Courses.$", course);
+
+            await _wishlistCollection.UpdateManyAsync(filter, update);
+        }
+
+        public async Task RemoveCourseInWishlistsAsync(string courseId)
+        {
+            var filter = Builders<Wishlist>.Filter.Eq("Courses._id", courseId);
+            var update = Builders<Wishlist>.Update
+                .Inc(x => x.Count, -1)
+                .PullFilter("Courses", Builders<Course>.Filter.Eq("_id", courseId));
+
+            await _wishlistCollection.UpdateManyAsync(filter, update);
+        }
+    }
+}
