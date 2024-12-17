27d9bf04b4319e12172b92b807292042ec7e70ec
Mạnh Vũ <vubamanh05@gmail.com>
2024-12-17 15:40:40 +0700

    Edit CategoriesSection in home page

--- frontend/vicourses_client_web/src/app/(public-sites)/(home)/page.tsx
+++ frontend/vicourses_client_web/src/app/(public-sites)/(home)/page.tsx
@@ -1,19 +1,9 @@
-import { Suspense } from "react";
 import CategoriesSection from "./CategoriesSection";
-import { Loader } from "@/components/common";
 
 export default function HomePage() {
     return (
         <div className="p-5">
-            <Suspense
-                fallback={
-                    <div className="flex justify-center items-center min-h-32">
-                        <Loader />
-                    </div>
-                }
-            >
-                <CategoriesSection />
-            </Suspense>
+            <CategoriesSection />
 
             <div>Home</div>
         </div>
