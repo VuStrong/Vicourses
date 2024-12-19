f5ea6c9bf00040006d74ea88051d3a383ba14e2f
Mạnh Vũ <vubamanh05@gmail.com>
2024-12-19 21:54:58 +0700

    Add README.md file

--- frontend/vicourses_client_web/src/app/(public-sites)/course/[title]/[id]/components/Sidebar.tsx
+++ frontend/vicourses_client_web/src/app/(public-sites)/course/[title]/[id]/components/Sidebar.tsx
@@ -1,13 +1,29 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { DEFAULT_COURSE_THUMBNAIL_URL } from "@/libs/constants";
 import { CourseDetail } from "@/libs/types/course";
 import OpenPreviewVideoModalButton from "./OpenPreviewVideoModalButton";
 import CoursePurchaseSection from "./CoursePurchaseSection";
 
 export default function Sidebar({ course }: { course: CourseDetail }) {
+    const [style, setStyle] = useState<string>("mt-10");
+
+    const handleScroll = () => {
+        if (window.scrollY > 350) {
+            setStyle("fixed top-10 ml-[40rem] xl:ml-[50rem] 2xl:ml-[60rem]");
+        } else {
+            setStyle("mt-10");
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <aside className="w-[350px] shadow-xl bg-white">
+        <aside className={`w-[20rem] xl:w-[22rem] shadow-xl bg-white h-fit hidden lg:block ${style}`}>
             <div
                 className="relative w-full border border-gray-700"
                 style={{
---
+++ images/screenshots.md
@@ -0,0 +1,33 @@
+# Screenshots
+
+#### Home
+
+![Screenshot](./screenshots/client_home_page.png)
+
+#### Courses
+
+![Screenshot](./screenshots/client_courses_page.png)
+
+#### Course Detail
+
+![Screenshot](./screenshots/client_course_detail_page.png)
+![Screenshot](./screenshots/client_course_detail_page_2.png)
+![Screenshot](./screenshots/client_course_detail_page_3.png)
+
+#### Learn
+
+![Screenshot](./screenshots/client_learn_video_page.png)
+![Screenshot](./screenshots/client_learn_quizzes_page.png)
+
+#### Course Management
+
+![Screenshot](./screenshots/client_course_management_page.png)
+![Screenshot](./screenshots/client_course_curriculum_management_page.png)
+![Screenshot](./screenshots/client_course_coupons_management_page.png)
+![Screenshot](./screenshots/client_course_goals_management_page.png)
+
+#### Instructor dashboard
+
+![Screenshot](./screenshots/client_instructor_courses_page.png)
+![Screenshot](./screenshots/client_instructor_performance_page.png)
+![Screenshot](./screenshots/client_instructor_ratings_page.png)
